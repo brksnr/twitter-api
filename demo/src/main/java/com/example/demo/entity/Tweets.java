8607451325fecc5fe947af5572ad3afabd438612
brksnr <berksener04@gmail.com>
2025-02-03 15:44:37 +0300

    tweets sınıfı oluşturuldu.

--- demo/src/main/java/com/example/demo/entity/Tweets.java
+++ demo/src/main/java/com/example/demo/entity/Tweets.java
@@ -1,4 +1,20 @@
 package com.example.demo.entity;
 
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "tweets", schema = "twitter")
 public class Tweets {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "content")
+    private String Content;
+    @Column(name = "created_at")
+    private Timestamp createdAt;
 }
