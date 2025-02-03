4740114f41a563e2aebf247d3a89894330b78631
brksnr <berksener04@gmail.com>
2025-02-03 15:50:00 +0300

    likes sınıfı oluşturuldu. many to one ilişki kuruldu.

--- demo/src/main/java/com/example/demo/entity/Likes.java
+++ demo/src/main/java/com/example/demo/entity/Likes.java
@@ -1,4 +1,25 @@
 package com.example.demo.entity;
 
+
+import com.example.demo.entity.user.User;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "likes", schema = "twitter")
 public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "tweet_id", nullable = false)
+    private Tweets tweets;
+
+
 }
