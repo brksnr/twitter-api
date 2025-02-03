a6d820b456d6b3e429267b8b5ae5e4332326f57c
brksnr <berksener04@gmail.com>
2025-02-03 19:27:25 +0300

    user service oluşturuldu.

---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username)
+                .orElseThrow(() -> {
+                    System.out.println("Kullanıcı bilgileri geçerli değil!");
+                    throw new UsernameNotFoundException("Kullanıcı bilgileri geçerli değil!");
+                });
+    }
+}
